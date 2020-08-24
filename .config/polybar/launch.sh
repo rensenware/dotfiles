b6a08be0470f28cfa148f8bb96c7130ed63e2aba
rensenware <50802625+rensenware@users.noreply.github.com>
2020-08-24 11:22:01 -0400

    Add files via upload

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload bar &
+done
+
+echo "Polybar launched..."
